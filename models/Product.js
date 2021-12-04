2416a22232d528215c79af1ae70c230035f5b25a
ChristianAnti <89366348+ChristianAnti@users.noreply.github.com>
2021-12-04 18:41:28 -0500

    updated models

--- models/Product.js
+++ models/Product.js
@@ -21,12 +21,19 @@ Product.init(
     },
     product_name: { type: DataTypes.STRING, allowNull: false },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
